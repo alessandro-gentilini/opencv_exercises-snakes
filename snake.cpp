6d1f45e3acc83ea4e262a37a102533feac68b403
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2014-07-12 15:13:51 +0200

    correct some errors

--- snake.cpp
+++ snake.cpp
@@ -11,9 +11,47 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <cmath>
+#include <string>
+
+std::string getImgType(int imgTypeInt)
+{
+    int numImgTypes = 35; // 7 base types, with five channel options each (none or C1, ..., C4)
+
+    int enum_ints[] =       {CV_8U,  CV_8UC1,  CV_8UC2,  CV_8UC3,  CV_8UC4,
+                             CV_8S,  CV_8SC1,  CV_8SC2,  CV_8SC3,  CV_8SC4,
+                             CV_16U, CV_16UC1, CV_16UC2, CV_16UC3, CV_16UC4,
+                             CV_16S, CV_16SC1, CV_16SC2, CV_16SC3, CV_16SC4,
+                             CV_32S, CV_32SC1, CV_32SC2, CV_32SC3, CV_32SC4,
+                             CV_32F, CV_32FC1, CV_32FC2, CV_32FC3, CV_32FC4,
+                             CV_64F, CV_64FC1, CV_64FC2, CV_64FC3, CV_64FC4};
+
+    std::string enum_strings[] = {"CV_8U",  "CV_8UC1",  "CV_8UC2",  "CV_8UC3",  "CV_8UC4",
+                             "CV_8S",  "CV_8SC1",  "CV_8SC2",  "CV_8SC3",  "CV_8SC4",
+                             "CV_16U", "CV_16UC1", "CV_16UC2", "CV_16UC3", "CV_16UC4",
+                             "CV_16S", "CV_16SC1", "CV_16SC2", "CV_16SC3", "CV_16SC4",
+                             "CV_32S", "CV_32SC1", "CV_32SC2", "CV_32SC3", "CV_32SC4",
+                             "CV_32F", "CV_32FC1", "CV_32FC2", "CV_32FC3", "CV_32FC4",
+                             "CV_64F", "CV_64FC1", "CV_64FC2", "CV_64FC3", "CV_64FC4"};
+
+    for(int i=0; i<numImgTypes; i++)
+    {
+        if(imgTypeInt == enum_ints[i]) return enum_strings[i];
+    }
+    return "unknown image type";
+}
+
+typedef std::vector<cv::Point2d> W_t;
+typedef float distance_t;
+
+template< typename T >
+struct CPP_TYPE_TO_OPENCV_IMG_TYPE
+{
+    CPP_TYPE_TO_OPENCV_IMG_TYPE() { assert(32==8*sizeof(distance_t)); }
+};
+template<> struct CPP_TYPE_TO_OPENCV_IMG_TYPE<distance_t> { enum { type = CV_32FC1 }; };
 
 
-double distance_from_edge( const cv::Mat& distance_img, const cv::Point2d w)
+double distance_from_edge( const cv::Mat& distance_img, const W_t::value_type& w)
 {
     if ( w.y >= distance_img.rows || w.x >= distance_img.cols ) {
         double minVal; 
@@ -21,12 +59,11 @@ double distance_from_edge( const cv::Mat& distance_img, const cv::Point2d w)
         cv::Point minLoc; 
         cv::Point maxLoc;
         cv::minMaxLoc( distance_img, &minVal, &maxVal, &minLoc, &maxLoc );
+        std::cout << "rotto!!!";
         return (pow(w.y,2)+pow(w.x,2))*maxVal;
     }
-    return distance_img.at<double>(w.y,w.x);
+    return distance_img.at<distance_t>(w.y,w.x);
 }
-
-typedef std::vector<cv::Point2d> W_t;
 
 double vector_norm2(const W_t::value_type& p)
 {
@@ -41,13 +78,17 @@ double space(const W_t& W, const size_t& N, const size_t& n)
         consecutive_distance_average += vector_norm2(W[i]-W[i-1]);
     }
     consecutive_distance_average /= N;
-    return -pow(consecutive_distance_average-vector_norm2(W[n]-W[n-1]),2);
+    double result = -pow(consecutive_distance_average-vector_norm2(W[n]-W[n-1]),2);
+    std::cout << "space\t" << result << "\n";
+    return result;
 }
 
 // Formula 17.6
 double curve(const W_t& W, const size_t& n)
 {
-    return -pow(vector_norm2(W[n-1]-2*W[n]+W[n+1]),2);
+    double result = -pow(vector_norm2(W[n-1]-2*W[n]+W[n+1]),2);
+    std::cout << "curve\t" << result << "\n";
+    return result;
 }
 
 // Formula 17.4
@@ -57,6 +98,7 @@ double prior(const W_t& W, const size_t& N, const double& alpha, const double& b
     for ( size_t n = 1; n <= N; n++ ) {
         result *= exp(alpha*space(W,N,n)+beta*curve(W,n));
     }
+    std::cout << "prior\t" << result << "\n";
     return result;
 }
 
@@ -65,7 +107,9 @@ double likelihood(const cv::Mat& distance_img, const W_t& W, const size_t& N)
 {
     double result = 1;
     for ( size_t n = 1; n <= N; n++ ) {
-        result *= exp(-pow(distance_from_edge(distance_img,W[n]),2));
+        double d = distance_from_edge(distance_img,W[n]);
+        result *= exp(-pow(d,2));;
+        std::cout << "likelihood d\t" << d << "\t" << -pow(d,2) << "\n";
     }
     return result;
 }
@@ -93,25 +137,30 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    cv::namedWindow( "image", cv::WINDOW_AUTOSIZE );
+    //cv::namedWindow( "image", cv::WINDOW_AUTOSIZE );
+    cv::namedWindow( "image" );
     cv::imshow( "image", image );                  
 
     cv::Mat edges;
     cv::Canny(image,edges,100,200);
+    edges = 255-edges;
 
     cv::namedWindow( "edges", cv::WINDOW_AUTOSIZE );
     cv::imshow( "edges", edges );                  
 
-    cv::Mat distance(edges.rows,edges.cols,CV_32FC1);
-    cv::distanceTransform(255-edges,distance,  CV_DIST_C,CV_DIST_MASK_PRECISE);
-
-    assert(32==4*sizeof(double));
+    cv::Mat distance(edges.rows,edges.cols,CPP_TYPE_TO_OPENCV_IMG_TYPE<distance_t>::type);
+    
+    cv::distanceTransform(edges,distance, CV_DIST_L2, CV_DIST_MASK_PRECISE);
+    std::ostringstream oss;
+    oss << distance;
+    std::cout << getImgType(distance.type()) << oss.str().substr(0,70) << "\n";
 
     cv::Mat normalized_dist;
-    cv::normalize(distance, normalized_dist, 0.0, 1.0, cv::NORM_MINMAX);
+    cv::normalize(distance.clone(), normalized_dist, 0.0, 1.0, cv::NORM_MINMAX);
 
     cv::namedWindow( "distance", cv::WINDOW_AUTOSIZE );
     cv::imshow( "distance", normalized_dist );    
+
 
     std::vector<cv::Vec3f> circles;
     cv::HoughCircles( edges, circles, CV_HOUGH_GRADIENT, 1, 10, 200, 25, 0, 0 );
